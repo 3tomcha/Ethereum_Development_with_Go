a69ea517e3f2b8b23d64a0cb4eb5ca564828a355
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-10-17 17:50:01 +0900

    succeeded read contract

---
+++ contract_read.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	store "github.com/3tomcha/Ethereum_Development_with_Go/contracts" // for demo
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress("0xb1ed820f4102143ea965d8301c10610488940d46")
+	instance, err := store.NewStore(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	version, err := instance.Version(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(version)
+}
